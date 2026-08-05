8b11662fdb32d6d9339d401e396612d6b0a1c562
katharye <multiden007@gmail.com>
2026-08-05 22:54:03 +0300

    create side enum

--- src/domain/model/__init__.py
+++ src/domain/model/__init__.py
@@ -1,2 +1,3 @@
 from domain.model.board import Board
 from domain.model.game import Game
+from domain.model.side import Side
---
+++ src/domain/model/side.py
@@ -0,0 +1,6 @@
+from enum import Enum, auto
+
+class Side(Enum):
+    PLAYER = auto()
+    MACHINE = auto()
+    DRAW = auto()
