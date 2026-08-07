99ebd65dd3ae15327993469458d6d362df9ac8df
katharye <multiden007@gmail.com>
2026-08-07 20:29:10 +0300

    add IBotStrategy

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,4 +1,4 @@
 # domain/__init__.py
 from domain.model import Game, Board, Side
-from domain.interfaces import IGameRepository
+from domain.interfaces import IGameRepository, IBotStrategy
 from domain.service import GameService, GameServiceABC, BotStrategy_MinMax
