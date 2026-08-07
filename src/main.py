d3c711f1794e55bef1ca4aff96e3add33cbdb826
katharye <multiden007@gmail.com>
2026-08-07 17:08:43 +0300

    add main.py

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+from di import Container, configure_container
+from domain import GameServiceABC
+from web.route.game_route import create_game_blueprint
+
+def create_app():
+    cn = Container()
+    configure_container(cn)
+
+    app = Flask(__name__)
+
+    game_service = cn.resolve(GameServiceABC)
+    game_bp = create_game_blueprint(game_service)
+    app.register_blueprint(game_bp)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, port=5000)
