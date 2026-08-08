c05031e31dba4beec51a5af4d1401bfb25c4bfc1
katharye <multiden007@gmail.com>
2026-08-08 16:46:13 +0300

    add signup request model

--- src/domain/model/__init__.py
+++ src/domain/model/__init__.py
@@ -2,3 +2,5 @@
 from domain.model.board import Board
 from domain.model.game import Game
 from domain.model.side import Side
+from domain.model.player import Player
+from domain.model.signup_request import SignUpRequest
