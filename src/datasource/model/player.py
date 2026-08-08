635ca03b70f7e4591cc28a43bf1885b8c02dd6ff
katharye <multiden007@gmail.com>
2026-08-08 16:43:29 +0300

    change tablename

--- src/datasource/model/player.py
+++ src/datasource/model/player.py
@@ -4,7 +4,7 @@ from datasource.db.base import Base
 
 
 class PlayerEntity(Base):
-    __tablename__ = "player"
+    __tablename__ = "players"
 
     player_id: Mapped[str] = mapped_column(String(36), unique=True, index=True, primary_key=True)
     player_login: Mapped[str] = mapped_column(String(50), unique=True, index=True)
