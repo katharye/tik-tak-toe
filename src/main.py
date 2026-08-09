2d63dd3d57227648e10c82c5a0be3ee0a7924c2f
katharye <multiden007@gmail.com>
2026-08-09 11:31:12 +0300

    add new auth service and authentification

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 from datasource.db import Base, engine
 from di import Container, configure_container
-from domain import GameServiceABC
-from web.route.game_route import create_game_blueprint
+from domain import GameServiceABC, IAuthService
+from web import create_game_blueprint, create_auth_blueprint, UserAuthenticator
 
 def create_app():
     Base.metadata.create_all(bind=engine)
@@ -16,6 +16,14 @@ def create_app():
     game_service = cn.resolve(GameServiceABC)
     game_bp = create_game_blueprint(game_service)
     app.register_blueprint(game_bp)
+
+    auth_service = cn.resolve(IAuthService)
+    auth_bp = create_auth_blueprint(auth_service)
+    app.register_blueprint(auth_bp)
+
+    authenticator = UserAuthenticator(auth_service)
+    authenticator.register(app, exempt_blueprints=["auth"])
+    
 
     return app
 
