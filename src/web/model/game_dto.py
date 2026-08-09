1b19bf3cf8e299c4bfb082cd9c80fdaafe5dfd80
katharye <multiden007@gmail.com>
2026-08-09 23:27:01 +0300

    updated

--- src/web/model/game_dto.py
+++ src/web/model/game_dto.py
@@ -8,22 +8,48 @@ from web.model.board_dto import BoardDTO
 class GameDTO:
     game_id: str
     board: BoardDTO
+    type: str
+    state: str
+    player_x_id: str
+    player_o_id: str
+    current_turn_id: str
 
     def to_dict(self) -> dict:
         return {
             "game_id": self.game_id,
-            "board": self.board.to_dict()
+            "board": self.board.to_dict(),
+            "type": self.type,
+            "state": self.state,
+            "player_x_id": self.player_x_id,
+            "player_o_id": self.player_o_id,
+            "current_turn_id": self.current_turn_id,
         }
 
     @classmethod
     def from_dict(cls, data: dict) -> Optional[Self]:
         game_id = data.get("game_id", None)
         board = data.get("board", None)
-        if game_id is None or board is None:
+
+        type = data.get("type", None)
+        state = data.get("state", None)
+
+        player_x_id = data.get("player_x_id", None)
+        player_o_id = data.get("player_o_id", None)
+        current_turn_id = data.get("current_turn_id", None)
+
+        if None in (game_id, board, type, state, player_x_id, player_o_id, current_turn_id):
             return None
 
         board_dto = BoardDTO.from_dict(board)
         if board_dto is None:
             return None
 
-        return GameDTO(game_id=game_id, board=board_dto)
+        return GameDTO(
+            game_id=game_id, 
+            board=board_dto,
+            type=type,
+            state=state,
+            player_x_id=player_x_id,
+            player_o_id=player_o_id,
+            current_turn_id=current_turn_id,
+            )
