db3df0fd3e69536c79b89870ae07078213f544fd
katharye <multiden007@gmail.com>
2026-08-09 11:29:16 +0300

    add domain <-> web mapper

---
+++ src/web/mapper/player_mapper.py
@@ -0,0 +1,23 @@
+# web/mapper/dto_mapper.py
+from domain import Player
+from web.model import PlayerDTO
+
+from uuid import UUID
+
+class WebPlayerMapper:
+
+    @classmethod
+    def to_web(cls, domain: Player) -> PlayerDTO: 
+        return PlayerDTO(
+            id=str(domain.player_id),
+            login=str(domain.login),
+            password=str(domain.password)
+        )
+
+    @classmethod
+    def to_domain(cls, web: PlayerDTO) -> Player: 
+        return Player (
+            player_id=UUID(web.id),
+            login=web.login,
+            password=web.password
+        )
