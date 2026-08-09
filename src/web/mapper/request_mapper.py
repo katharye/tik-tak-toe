ee4ac8ed93682e91901047396cf0a930d90b47ca
katharye <multiden007@gmail.com>
2026-08-09 11:30:12 +0300

    add domain <-> web request mapper

---
+++ src/web/mapper/request_mapper.py
@@ -0,0 +1,21 @@
+# web/mapper/dto_mapper.py
+from domain import SignUpRequest
+from web.model import SignUpRequestDTO
+
+from uuid import UUID
+
+class WebSignUpRequestMapper:
+
+    @classmethod
+    def to_web(cls, domain: SignUpRequest) -> SignUpRequestDTO: 
+        return SignUpRequestDTO(
+            login=domain.login,
+            password=domain.password
+        )
+
+    @classmethod
+    def to_domain(cls, web: SignUpRequestDTO) -> SignUpRequest: 
+        return SignUpRequest (
+            login=web.login,
+            password=web.password
+        )
