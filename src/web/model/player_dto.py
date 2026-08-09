c72609a7841ae0bc188e6353b5fc305edada7b1d
katharye <multiden007@gmail.com>
2026-08-09 11:27:08 +0300

    add player dto model

---
+++ src/web/model/player_dto.py
@@ -0,0 +1,31 @@
+# web/model/board_dto.py
+from typing import Self, Optional
+from dataclasses import dataclass
+
+@dataclass
+class PlayerDTO:
+    id: str
+    login: str
+    password: str
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "login": self.login,
+            "password": self.password,
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Optional[Self]:
+        id = data.get("id")
+        login = data.get("login")
+        password = data.get("password")
+        if None in (id, login, password):
+            return None
+        
+        return PlayerDTO(
+            id=id, 
+            login=login,
+            password=password
+        )
+    
