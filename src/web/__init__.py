523f93c36b7e5a99ed20449eabc097e8fe6e36c2
katharye <multiden007@gmail.com>
2026-08-09 23:28:08 +0300

    updated

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,4 +1,4 @@
 # web/__init__.py
 from .route import GameRoute
-from .route import create_game_blueprint, create_auth_blueprint
+from .route import create_game_blueprint, create_auth_blueprint, create_user_blueprint
 from .authenticator import UserAuthenticator
