3fd504612d61263af897f986da62c5d28b89dac6
katharye <multiden007@gmail.com>
2026-08-09 11:23:23 +0300

    add signup request

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,4 +1,4 @@
 # domain/__init__.py
-from domain.model import Game, Board, Side, Player
+from domain.model import Game, Board, Side, Player, SignUpRequest
 from domain.interfaces import IGameRepository, IBotStrategy, IPlayerRepository
 from domain.service import GameService, GameServiceABC, BotStrategy_MinMax, AuthService, IAuthService
