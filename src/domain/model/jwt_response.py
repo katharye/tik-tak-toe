635f0f204d2622721ff3af2d05972dd05bdd78c6
katharye <multiden007@gmail.com>
2026-08-10 11:21:45 +0300

    tokens attributes now optional

--- src/domain/model/jwt_response.py
+++ src/domain/model/jwt_response.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass
 class JWTResponce:
     type: str
-    access_token: str
-    refresh_token: str
+    access_token: Optional[str]
+    refresh_token: Optional[str]
